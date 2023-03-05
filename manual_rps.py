67db7dfec6d5e2b9e986741690b9765b2deac021
Matthew Marquis <matthew_marquis@btinternet.com>
2023-03-05 17:06:00 +0000

    Added if-else logic to get winner

--- manual_rps.py
+++ manual_rps.py
@@ -8,3 +8,16 @@ def get_computer_choice():
 def get_user_choice():
     user_choice = input("Please enter 'Rock', 'Paper' or 'Scissors' to play: ")
     return user_choice
+
+def get_winner(computer_choice, user_choice):
+    if computer_choice == 'Rock' and user_choice == 'Scissors':
+        print('You lost')
+    elif computer_choice == 'Paper' and user_choice == 'Rock':
+        print('You lost')     
+    elif computer_choice == 'Scissors' and user_choice == 'Paper':
+        print('You lost')
+    elif computer_choice == user_choice:
+        print('It is a tie!')
+    else:
+        print('You won!')
+    
