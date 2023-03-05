3424ad68e51eb136f015cf7c3dd3d5017af2873c
Matthew Marquis <matthew_marquis@btinternet.com>
2023-03-05 17:09:44 +0000

    Added and called play function

--- manual_rps.py
+++ manual_rps.py
@@ -20,4 +20,8 @@ def get_winner(computer_choice, user_choice):
         print('It is a tie!')
     else:
         print('You won!')
-    
+
+def play():
+    get_winner(get_computer_choice(), get_user_choice())
+
+play()
