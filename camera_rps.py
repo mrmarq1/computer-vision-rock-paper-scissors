5eb2c6666bad1228c4b431c26008d1d9b56c7a2e
Matthew Marquis <matthew_marquis@btinternet.com>
2023-03-07 12:07:57 +0000

    Created num_rounds and countdown_time parameters and refactored code with class introduced

--- camera_rps.py
+++ camera_rps.py
@@ -1,47 +1,81 @@
 import cv2
 from keras.models import load_model
 import numpy as np
-import time
+from time import sleep
 from threading import Thread, Event
+from random import choice
 
-model = load_model('keras_model.h5')
-cap = cv2.VideoCapture(0)
-data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)       
-vid_input = False
+class Camera_RPS:
+   MODEL = load_model('keras_model.h5')
+   OPTIONS = ('Rock', 'Paper', 'Scissors','Nothing')
+   
+   def __init__(self, num_rounds=3, countdown_time=3):
+      self.num_rounds = num_rounds
+      self.countdown_time = countdown_time
+      self.vid_input = False
+      self.computer_wins, self.user_wins = 0, 0 
+      self.data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32) 
+      self.cap = cv2.VideoCapture(0)  
+   
+   def play(self):
+      event = Event()
+      thread = Thread(target=self.get_user_input, args=(event,))
+      thread.start()
+      while not self.vid_input: pass
+      while True:
+         self.get_countdown(self.countdown_time)
+         self.get_round_winner(self.get_computer_choice(), self.get_prediction(self.data))
+         game_over_scenarios = {self.computer_wins: 'Game over... the computer won.',
+                                self.user_wins: 'Congratulations... you won the game!'}
+         game_over_message = game_over_scenarios.get(self.num_rounds)
+         if game_over_message:
+            print(game_over_message)
+            event.set()
+            break
 
-def countdown(seconds):
-  for countdown in range(seconds,0,-1):
-     print(f'{countdown}...')
-     time.sleep(1)
+   def get_countdown(self, countdown_time):
+      for countdown in range(countdown_time,0,-1):
+         print(f'{countdown}...')
+         sleep(1)
 
-def get_user_input(event):    
-    global vid_input, cap
-    while True:
-      ret, frame = cap.read()
-      resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
-      image_np = np.array(resized_frame)
-      normalized_image = (image_np.astype(np.float32) / 127.0) - 1
-      data[0] = normalized_image
-      cv2.imshow('frame', frame)
-      vid_input = True
-      if cv2.waitKey(1) & 0xFF == ord('q'):
-         break
-      if event.is_set():
-         break
-      
-def get_prediction(data):
-   prediction = model.predict(data)
-   options = ('Rock', 'Paper', 'Scissors', 'Nothing')
-   print(f'Prediction: {options[int(prediction.argmax(axis=1))]}')
+   def get_user_input(self, event):    
+      while True:
+         ret, frame = self.cap.read()
+         resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
+         image_np = np.array(resized_frame)
+         normalized_image = (image_np.astype(np.float32) / 127.0) - 1
+         self.data[0] = normalized_image
+         cv2.imshow('frame', frame)
+         self.vid_input = True
+         if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+         if event.is_set():
+            break
+         
+   def get_prediction(self, data):
+      predictions_arr = Camera_RPS.MODEL.predict(data)
+      prediction = Camera_RPS.OPTIONS[int(predictions_arr.argmax(axis=1))]
+      print(f'Your choice: {prediction}')
+      return prediction
 
-def play():
-  event = Event()
-  thread = Thread(target=get_user_input, args=(event,))
-  thread.start()
-  while not vid_input: pass
-  for _ in range(3):
-    countdown(3)
-    get_prediction(data)	
-  event.set()
+   def get_computer_choice(self):
+      computer_choice = choice(Camera_RPS.OPTIONS[:3])
+      print(f'Computer choice: {computer_choice}')
+      return computer_choice
 
-play()
+   def get_round_winner(self, computer_choice, user_choice):      
+      loss_scenarios = {'Rock': 'Scissors', 'Paper': 'Rock', 'Scissors': 'Paper'}
+
+      if user_choice == 'Nothing':
+         print('You didn\'t make a choice. Try again.') 
+      elif loss_scenarios[computer_choice] == user_choice:
+         print('You lost the round')
+         self.computer_wins += 1
+      elif computer_choice == user_choice:
+         print('It is a tie!')
+      else:
+         print('You won the round!')
+         self.user_wins += 1
+
+game1 = Camera_RPS()
+game1.play()
